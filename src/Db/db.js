d1438c7847f569bc65a0b1c8ee88201755eb8c3c
sanjaysingh24 <sanjubora84@gmail.com>
2026-01-12 10:50:18 +0530

    update

--- src/Db/db.js
+++ src/Db/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 export const connectdb = async()=>{
     try{
-       let connectdata = await mongoose.connect(`${process.env.MONGODB_URL}`);  
+       let connectdata = await mongoose.connect(`${process.env.MONGODB_URL,{
+        serverSelectionTimeoutMS: 5000
+       }}`);  
        if(connectdata){
          console.log("database successfully connected")
        }
